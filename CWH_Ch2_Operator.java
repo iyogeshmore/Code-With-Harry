dc4f177d52cf4a28a5f265b1a9c0afcf2e67986b
iyogeshmore <iyogesh.more1998@gmail.com>
2022-04-15 20:22:22 +0530

    Added Operator

---
+++ CWH_Ch2_Operator.java
@@ -0,0 +1,11 @@
+package FirstJavaProgramCWH;
+
+public class CWH_Ch2_Operator {
+    public static void main(String[] args) {
+        int a = 4 ;
+        int b = 7 + a ;
+
+        System.out.println(b);
+        System.out.println(8==8); // Comparison operator
+    }
+}
