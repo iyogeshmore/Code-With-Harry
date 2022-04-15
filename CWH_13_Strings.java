0e3af27e6817a1cf54337bbbbf2816db85993407
iyogeshmore <iyogesh.more1998@gmail.com>
2022-04-15 20:16:32 +0530

    Added string

---
+++ CWH_13_Strings.java
@@ -0,0 +1,8 @@
+package FirstJavaProgramCWH;
+
+public class CWH_13_Strings {
+    public static void main(String[] args) {
+        String name = new String("Yogesh");
+        System.out.println(name);
+    }
+}
